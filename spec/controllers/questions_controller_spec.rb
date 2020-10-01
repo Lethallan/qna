957b20f952d3c28446741d8701145f7124ab0b06
Daria Korytova <dk17march@gmail.com>
2020-10-01 22:34:14 +0300

    questions controller actions with spec

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
-  describe 'GET index' do
+  let(:question) { create(:question) }
+
+  describe 'GET #index' do
     let(:questions) { create_list(:question, 3) }
     before { get :index }
 
@@ -11,6 +13,116 @@ RSpec.describe QuestionsController, type: :controller do
 
     it 'renders index view' do
       expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    before { get :show, params: { id: question } }
+
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'assigns a new question to @question' do
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET #edit' do
+    before { get :edit, params: { id: question } }
+
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders edit view' do
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves a new question in the database' do
+        expect { post :create, params: { question: attributes_for(:question) } }.to change(Question, :count).by(1)
+      end
+
+      it 'redirects to show' do
+        post :create, params: { question: attributes_for(:question) }
+        expect(response).to redirect_to assigns(:question)
+      end
+    end
+
+    context 'with wrong attributes' do
+      it 'does not save the question' do
+        expect { post :create, params: { question: attributes_for(:question, :wrong) } }.to_not change(Question, :count)
+      end
+
+      it 'renders new view again' do
+        post :create, params: { question: attributes_for(:question, :wrong) }
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+    context 'with valid attributes' do
+      it 'assigns the requested question to @question' do
+        patch :update, params: { id: question, question: attributes_for(:question) }
+        expect(assigns(:question)).to eq question
+      end
+
+      it 'changes question attributes' do
+        patch :update, params: { id: question, question: { title: 'new title', body: 'new text' } }
+        question.reload
+
+        expect(question.title).to eq 'new title'
+        expect(question.body).to eq 'new text'
+      end
+
+      it 'redirects to updated question' do
+        patch :update, params: { id: question, question: attributes_for(:question) }
+        expect(response).to redirect_to question
+      end
+    end
+
+    context 'with wrong attributes' do
+      before { patch :update, params: { id: question, question: attributes_for(:question, :wrong) } }
+
+      it 'does not change the question' do
+        question.reload
+
+        expect(question.title).to eq 'Question title'
+        expect(question.body).to eq 'Question text'
+      end
+
+      it 'renders edit view again' do
+        expect(response).to render_template :edit
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let!(:question) { create(:question) }
+
+    it 'deletes the question' do
+      expect { delete :destroy, params: { id: question } }.to change(Question, :count).by(-1)
+    end
+
+    it 'redirects to index' do
+      delete :destroy, params: { id: question }
+      expect(response).to redirect_to questions_path
     end
   end
 end
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :question do
     title { "Question title" }
     body { "Question text" }
+
+    trait :wrong do
+      title { nil }
+    end
   end
 end

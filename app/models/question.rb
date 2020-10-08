ab99085e968dbad04f953db10a99e6b80f87c5cd
Daria Korytova <dk17march@gmail.com>
2020-10-08 19:01:37 +0300

    added references for questions and anwers to users

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ApplicationRecord
   has_many :answers
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id', optional: true
 
   validates :title, :body, presence: true
 end

37650cdb541391253fec0717af975d0f98092207
Daria Korytova <dk17march@gmail.com>
2020-10-08 20:57:02 +0300

    specs for user model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
   has_many :answers
-  belongs_to :author, class_name: 'User', foreign_key: 'user_id', optional: true
+  belongs_to :user, foreign_key: :user_id
 
   validates :title, :body, presence: true
 end

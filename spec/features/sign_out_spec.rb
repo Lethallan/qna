234b235bb324a63a5ea07afbcd6a193a5adba053
Daria Korytova <dk17march@gmail.com>
2020-10-13 18:53:01 +0300

    usep acceptances ok

--- spec/features/sign_out_spec.rb
+++ spec/features/sign_out_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
-# feature 'User can sign out', %q{
-#   In order to finish session
-#   As an authenticated user
-#   I'd like to be able to sign out
-# } do
+feature 'User can sign out', %q{
+  In order to finish session
+  As an authenticated user
+  I'd like to be able to sign out
+} do
 
-#   given(:user) { create(:user) }
+  given(:user) { create(:user) }
 
-#   scenario 'Registered user tries to sign out' do
-#     sign_in(user)
+  scenario 'Registered user tries to sign out' do
+    sign_in(user)
 
-#     click_on 'Sign out'
+    click_on 'Sign out'
 
-#     expect(page).to have_content 'Signed out successfully.'
-#   end
-# end
+    expect(page).to have_content 'Signed out successfully.'
+  end
+end

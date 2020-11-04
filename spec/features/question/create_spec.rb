2f749258cf0ffe070f4fad819ec352a00d4f12e0
Daria Korytova <dk17march@gmail.com>
2020-11-04 20:07:42 +0300

    add many files

--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -35,10 +35,11 @@ feature 'User can create questions', %q{
       fill_in 'Title', with: 'Question title'
       fill_in 'Body', with: 'Bla bla bla'
 
-      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+      attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
       click_on 'Submit'
 
       expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
     end
   end
 

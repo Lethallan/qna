e95654e94b8a1702465c8e0cda1a610ec7cc407f
Daria Korytova <dk17march@gmail.com>
2020-11-10 00:40:15 +0300

    links

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -12,6 +12,7 @@ class QuestionsController < ApplicationController
 
   def new
     @question = Question.new
+    @question.links.new
   end
 
   def edit
@@ -48,6 +49,9 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    params.require(:question).permit(:title, :body, files: [])
+    params.require(:question).permit(:title,
+                                     :body,
+                                     files: [],
+                                     links_attributes: [:name, :url])
   end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
   context 'associations' do
     it { should have_many(:answers).dependent(:destroy) }
+    it { should have_many(:links).dependent(:destroy) }
     it { should belong_to :user }
   end
 
@@ -15,5 +16,9 @@ RSpec.describe Question, type: :model do
     it 'have many attached files' do
       expect(Question.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
     end
+  end
+
+  context 'nested attributes' do
+    it { should accept_nested_attributes_for :links }
   end
 end
